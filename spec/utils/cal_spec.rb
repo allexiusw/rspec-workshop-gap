b4e30470ac8cdb0de455709d12bc9642bf26e5fd
allex <william.al1379@gmail.com>
2023-05-16 23:10:49 -0600

    Cover basic structure of a test and matchers

--- spec/utils/cal_spec.rb
+++ spec/utils/cal_spec.rb
@@ -3,8 +3,15 @@
 require './src/utils/calc'
 
 describe Calc do
-  context 'with parameters valid' do
-    it 'should return True' do
+  context 'with invalid parameters' do
+    it 'valid? should return false' do
+      subject = described_class.new(-1, 1)
+      expect(subject.valid?).to be_falsey
+    end
+
+    it 'valid should raise exception' do
+      subject = described_class.new(-1, 1)
+      expect { subject.valid }.to raise_error(StandardError)
     end
   end
 end
--- src/utils/calc.rb
+++ src/utils/calc.rb
@@ -2,5 +2,36 @@
 
 # Class base calc
 class Calc
-  'Small demo cal'
+  class ValidateNumberError < StandardError; end
+
+  attr_reader :number1, :number2
+
+  def initialize(number1, number2)
+    @number1 = number1
+    @number2 = number2
+  end
+
+  def valid?
+    number1 >= 0 && number2 >= 0
+  end
+
+  def valid
+    raise ValidateNumberError('Invalid number') unless valid?
+  end
+
+  def sum
+    number1 + number2
+  end
+
+  def subtract
+    number1 - number2
+  end
+
+  def multiply
+    number1 * number2
+  end
+
+  def divide
+    number1 / number2
+  end
 end
